b942ad3307329c6c9e888d8de2dc717b8b9929e4
Isaac Martins <91567120+IsaacMartins12@users.noreply.github.com>
2026-09-19 14:53:42 -0400

    Implement UART receiver and LED control
    Added UART communication setup and LED control logic.

--- comunicao_serial_uart_entre_dois_esps/esp32_receiver.ino
+++ comunicao_serial_uart_entre_dois_esps/esp32_receiver.ino
@@ -18,3 +18,50 @@ void loop() {
     Serial.println(dadosRecebidos);
   }
 }
+
+
+// Esse debaixo é para comunicar e ligar um led
+
+/*
+
+#include <Arduino.h>
+
+#define RX2_PIN 16
+#define TX2_PIN 17
+#define LED_PIN 2
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RX2_PIN, TX2_PIN);
+  
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Garante estado inicial desligado
+
+  Serial.println("Receptor UART Inicializado. Aguardando dados...");
+}
+
+void loop() {
+  if (Serial2.available() > 0) {
+    String dadosRecebidos = Serial2.readStringUntil('\n');
+    dadosRecebidos.trim(); // Remove '\r', espaços e quebras residuais
+
+    if (dadosRecebidos == "ligar led") {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("Led Ligado");
+    } else if (dadosRecebidos == "desligar led") {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("Led desligado");
+    }
+
+    Serial.print("Recebido: ");
+    Serial.println(dadosRecebidos);
+  }
+}
+
+
+
+
+
+
+
+*/
